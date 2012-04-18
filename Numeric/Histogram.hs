6ef3a0a46214fa1625ddd67aa8ddfaefe534961e
Ben Gamari <bgamari.foss@gmail.com>
2012-04-18 11:34:05 -0400

    Break out histWithBins

--- Numeric/Histogram.hs
+++ Numeric/Histogram.hs
@@ -1,22 +1,30 @@
-module Numeric.Histogram (binBounds, histValues) where
+module Numeric.Histogram ( Range
+                         , binBounds
+                         , histValues
+                         , histWithBins
+                         ) where
 
 import qualified Data.Vector as V
 import qualified Data.Vector.Mutable as MV
 import Control.Monad.ST
 import Control.Monad (foldM)
 
+type Range a = (a,a)
+
 -- | 'binBounds a b n' generates bounds for 'n' bins spaced linearly between
 -- 'a' and 'b'
-binBounds :: RealFrac a => a -> a -> Int -> [(a,a)]
+binBounds :: RealFrac a => a -> a -> Int -> [Range a]
 binBounds a b n = map (\i->(lbound i, lbound (i+1))) [0..n]
         where lbound i = a + (b-a) * realToFrac i / realToFrac n
 
 -- | 'histValues a b n vs' returns the histogram values for the histogram of
 -- 'vs' on the range from 'a' to 'b' with 'n' bins
-histValues :: RealFrac a => a -> a -> Int -> [a] -> [((a,a), Int)]
-histValues a b n vs =
-        let bins = binBounds a b n
-            testBin :: RealFrac a => a -> (a,a) -> Bool
+histValues :: RealFrac a => a -> a -> Int -> [a] -> [(Range a, Int)]
+histValues a b n = histWithBins (binBounds a b n)
+
+histWithBins :: RealFrac a => [Range a] -> [a] -> [(Range a, Int)]
+histWithBins bins xs =
+        let testBin :: RealFrac a => a -> Range a -> Bool
             testBin x (a,b) = x >= a && x < b
 
             --f :: RealFrac a => MV.STVector () Int -> a -> ST () (MV.STVector () Int)
@@ -26,9 +34,7 @@ histValues a b n vs =
                                                    MV.write bs idx (n+1)
                                                    return bs
 
-            counts = runST (do b <- MV.replicate (length bins) 0
-                               b' <- foldM f b vs
-                               b'' <- V.freeze b'
-                               return $ V.toList b'')
+            counts = runST $ do b <- MV.replicate (length bins) 0
+                                b' <- foldM f b xs
+                                (return . V.toList) =<< V.freeze b'
         in zip bins counts
-
