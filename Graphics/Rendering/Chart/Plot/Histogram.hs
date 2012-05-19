ca1d896a5324495babff12b4fa5fedce8993c231
Ben Gamari <bgamari.foss@gmail.com>
2012-05-19 19:29:41 -0400

    Histogram: Add normalized form

--- Graphics/Rendering/Chart/Plot/Histogram.hs
+++ Graphics/Rendering/Chart/Plot/Histogram.hs
@@ -3,6 +3,7 @@
 module Graphics.Rendering.Chart.Plot.Histogram ( PlotHist (..)
                                                , defaultPlotHist
                                                , plotHist
+                                               , plotNormedHist
                                                  
                                                , plot_hist_item_styles
                                                , plot_hist_bins
@@ -30,8 +31,8 @@ defaultPlotHist = PlotHist { plot_hist_item_styles_ = plot_bars_item_styles_ (de
                            , plot_hist_values_ = []
                            }
         
-histToBars :: RealFrac x => PlotHist x -> PlotBars x Int
-histToBars hist =
+histToBars' :: (RealFrac x, BarsPlotValue a) => (Double -> Int -> a) -> PlotHist x -> PlotBars x a
+histToBars' normalizeFunc hist =
         defaultPlotBars { plot_bars_item_styles_ = plot_hist_item_styles_ hist
                         , plot_bars_values_ = values'
                         , plot_bars_spacing_ = BarsFixGap 0 0
@@ -43,12 +44,24 @@ histToBars hist =
               dmax = maximum $ map maximum values
               (a,b) = maybe (dmin,dmax) id $ plot_hist_range_ hist
               n = plot_hist_bins_ hist
+              dx = realToFrac (b-a) / realToFrac n
               bounds = binBounds a b n
-              counts = map (map snd . histWithBins bounds) values
+              counts = map (\v->map (normalizeFunc (dx*realToFrac (length v)) . snd)
+                               $ histWithBins bounds v
+                           ) values
               values' = zip (map fst bounds) (transpose counts)
+
+histToBars :: RealFrac x => PlotHist x -> PlotBars x Int
+histToBars = histToBars' (const id)
+
+histToNormedBars :: RealFrac x => PlotHist x -> PlotBars x Double
+histToNormedBars = histToBars' (\norm n->realToFrac n / norm)
 
 plotHist :: RealFrac x => PlotHist x -> Plot x Int
 plotHist = plotBars . histToBars
+
+plotNormedHist :: RealFrac x => PlotHist x -> Plot x Double
+plotNormedHist = plotBars . histToNormedBars
 
 $( deriveAccessors ''PlotHist )
   
