2b41494dcf938fce6f8438efdaa82fe15670acd2
Ben Gamari <bgamari.foss@gmail.com>
2012-06-01 15:23:22 -0400

    Test: Changes

--- Test.hs
+++ Test.hs
@@ -5,14 +5,17 @@ import Graphics.Rendering.Chart
 import Graphics.Rendering.Chart.Gtk
 import Graphics.Rendering.Chart.Plot.Histogram
 
-values = [[1,1,2,3, 8,8,8,8]] :: [[Double]]
+values = [[1,1,2,3, 8,8,8,8, 10]] :: [[Double]]
 
 chart = layout
         where hist = plot_hist_values  ^= values
                      $ plot_hist_range ^= Just (0, 10)
+                     $ plot_hist_bins  ^= 10
                      $ defaultPlotHist
+              layout :: Layout1 Double Int
               layout = layout1_title ^= "Hello World"
-                     $ layout1_plots ^= [Left (histToBarsPlot hist)]
+                     $ layout1_plots ^= [ Left (histToLinesPlot hist)
+                                        ]
                      $ defaultLayout1
 
 main = do renderableToWindow (toRenderable chart) 640 480
